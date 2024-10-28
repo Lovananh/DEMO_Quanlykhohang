744000d7b59b483ba7103dc71565679c9db25dce
Lò Văn Anh <anhlo2819@gmail.com>
2024-10-28 17:28:42 +0700

    Them file

---
+++ src/Database/Quanlykhohang.java
@@ -0,0 +1,22 @@
+package Database;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Quanlykhohang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
